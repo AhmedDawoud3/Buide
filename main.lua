c653173b0747be62d069b2a7cafa445713ba72b0
Ahmed Dawoud <68483546+AhmedDawoud3@users.noreply.github.com>
2021-07-04 03:10:51 +0200

    main.lua optimization

--- main.lua
+++ main.lua
@@ -6,47 +6,54 @@
 
     Credit for music:
     https://freesound.org/people/Erokia/sounds/183881/
-]]
-
+]] --
 require 'src.Dependencies'
 require "imgui"
 
-fonts = {
-    ['Regular13'] = love.graphics.newFont('fonts/MontserratRegular.ttf', 13),
-    ['Bold16'] = love.graphics.newFont('fonts/MontserratBold.ttf', 16),
-    ['Bold32'] = love.graphics.newFont('fonts/MontserratBold.ttf', 32),
-    ['Medium40'] = love.graphics.newFont('fonts/MontserratMedium.ttf', 40),
-    ['Medium20'] = love.graphics.newFont('fonts/MontserratMedium.ttf', 20),
-    ['Semibold40'] = love.graphics.newFont('fonts/MontserratSemibold.ttf', 40),
-    ['ExtraBold60'] = love.graphics.newFont('fonts/MontserratExtrabold.ttf', 60),
-    ['ExtraBold100'] = love.graphics.newFont('fonts/MontserratExtrabold.ttf', 100),
-    ['ExtraBoldItalic100'] = love.graphics.newFont('fonts/MontserratExtraboldItalic.ttf', 100),
-    ['ExtraBoldItalic60'] = love.graphics.newFont('fonts/MontserratExtraboldItalic.ttf', 60),
-    ['symbol'] = love.graphics.newFont('fonts/symbola.ttf', 30)
-}
-
-sounds = {
-    ['main'] = love.audio.newSource('assets/sounds/main.wav', 'static')
-}
-
-levelsImages = {love.graphics.newImage('assets/images/Levels/1.png'),
-                love.graphics.newImage('assets/images/Levels/2.png')}
-
 function love.load()
-    love.window.setTitle("Buide")
-    options = LoadSaveFile()
+
+    -- seed the RNG so that calls to random are always random
     math.randomseed(os.time())
+
+    -- set the application title bar
+    love.window.setTitle("Buide")
+
+    options = LoadSaveFile()
+
+    -- initialize our fonts
+    fonts = {
+        ['Regular'] = love.graphics.newFont('fonts/MontserratRegular.ttf'),
+        ['Regular13'] = love.graphics.newFont('fonts/MontserratRegular.ttf', 13),
+        ['Bold16'] = love.graphics.newFont('fonts/MontserratBold.ttf', 16),
+        ['Bold32'] = love.graphics.newFont('fonts/MontserratBold.ttf', 32),
+        ['Medium40'] = love.graphics.newFont('fonts/MontserratMedium.ttf', 40),
+        ['Medium20'] = love.graphics.newFont('fonts/MontserratMedium.ttf', 20),
+        ['Semibold40'] = love.graphics.newFont('fonts/MontserratSemibold.ttf', 40),
+        ['ExtraBold60'] = love.graphics.newFont('fonts/MontserratExtrabold.ttf', 60),
+        ['ExtraBold100'] = love.graphics.newFont('fonts/MontserratExtrabold.ttf', 100),
+        ['ExtraBoldItalic100'] = love.graphics.newFont('fonts/MontserratExtraboldItalic.ttf', 100),
+        ['ExtraBoldItalic60'] = love.graphics.newFont('fonts/MontserratExtraboldItalic.ttf', 60),
+        ['symbol'] = love.graphics.newFont('fonts/symbola.ttf', 30)
+    }
+    love.graphics.setFont(fonts['Regular'])
+
+    -- initialize our resolution, which will be rendered within our
+    -- actual window no matter its dimensions
     push:setupScreen(Window.width, Window.height, Window.width, Window.height, {
         fullscreen = options.FullScreen,
         resizable = true,
         vsync = true
     })
+
+    -- the state machine we'll be using to transition between various states
+    -- in our game instead of clumping them together in our update and draw
+    -- methods
     gStateMachine = StateMachine {
-        ['play'] = function()
-            return PlayState()
-        end,
         ['start'] = function()
             return StartState()
+        end,
+        ['play'] = function()
+            return PlayState()
         end,
         ['about'] = function()
             return AboutState()
@@ -61,24 +68,38 @@ function love.load()
     }
     gStateMachine:change('start')
 
+    sounds = {
+        ['main'] = love.audio.newSource('assets/sounds/main.wav', 'static')
+    }
+
+    -- Load The Levels thumbnail for level select screen
+    levelsImages = {love.graphics.newImage('assets/images/Levels/1.png'),
+                    love.graphics.newImage('assets/images/Levels/2.png')}
+
     -- a table we'll use to keep track of which keys have been pressed this
     -- frame, to get around the fact that Love's default callback won't let us
     -- test for input from within other functions
     love.keyboard.keysPressed = {}
     love.mouse.keysPressed = {}
     love.mouse.keysReleased = {}
-    -- SaveGame()
+
+    -- play our music outside of all states and set it to looping
     sounds['main']:play()
+    sounds['main']:setLooping(true)
 end
 
 function love.update(dt)
     -- we pass in dt to the state object we're currently using
     gStateMachine:update(dt)
 
+    --[[
+        IMGUI debugging
+    ]]
     imgui.NewFrame()
 end
 
 function love.draw()
+    -- Starting push to scale all the renders according to the window dimensions
     push:apply('start')
     gStateMachine:render()
     DisplayFPS()
